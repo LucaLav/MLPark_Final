a7808a8ed1af72531f8af39b7a25e3515d835420
capuan <capone374@gmail.com>
2017-01-05 15:54:54 +0100

    Scatto con foto aggiunto

--- app/src/main/java/com/capone/lavorato/mlpark/MyDialog.java
+++ app/src/main/java/com/capone/lavorato/mlpark/MyDialog.java
@@ -4,15 +4,28 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by capuan on 05/01/17.
  */
 
 public class MyDialog extends DialogFragment{
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
 
     @Override
     public Dialog onCreateDialog (Bundle savedInstanceState){
@@ -25,7 +38,8 @@ public class MyDialog extends DialogFragment{
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
-                Log.i("info","Ti scatterò una foto");
+                //Apri fotocamera e scata foto
+                dispatchTakePictureIntent();
 
             }
         })
@@ -37,6 +51,48 @@ public class MyDialog extends DialogFragment{
                 });
 
         return builder.create();
+    }
+
+    //FOTO - INTENT che gestisce
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this.getContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+    //Crea File Immagine
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
